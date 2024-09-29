c058cf6b5ff2b159e9d97d4525854d3f5c7ca3e7
Hossein Khezeli <hosseinkhezli73@gmail.com>
2024-09-29 16:06:01 +0330

    update:clean up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="fa" dir="rtl">
       <body className={`${iranSans.className}`}>{children}</body>
     </html>
   );

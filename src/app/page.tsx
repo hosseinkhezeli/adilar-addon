fb41e2275cbfd5b4bf024a136f163aa6bf54842c
Hossein Khezeli <hosseinkhezli73@gmail.com>
2024-10-01 18:33:26 +0330

    update:testing design system

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,61 @@
+'use client';
 import React from 'react';
-import { Box } from '@mui/material';
+import { Button, Stack, Typography } from '@mui/material';
+import { InputListWithUseForm } from 'ideep-design-system-2';
+import { IUseFormInput } from 'ideep-design-system-2/components/input-list/type';
+import { useForm } from 'react-hook-form';
 
 export default function Home() {
-  return <Box flexGrow={1}>سنیتبا سیبنتاسی بسی</Box>;
+  const inputList: IUseFormInput[] = [
+    {
+      label: 'نام',
+      name: 'fullName',
+      type: 'text',
+      gridItemProp: { xs: 12 },
+      labelProps: { fontSize: 14, marginBottom: 0 },
+    },
+    {
+      label: 'نام خانوادگی',
+      name: 'fullName',
+      type: 'text',
+      gridItemProp: { xs: 12 },
+      labelProps: { fontSize: 14, marginBottom: 0 },
+    },
+    {
+      label: 'ایمیل',
+      name: 'fullName',
+      type: 'text',
+      gridItemProp: { xs: 12 },
+      labelProps: { fontSize: 14, marginBottom: 0 },
+    },
+    {
+      label: 'شماره موبایل',
+      name: 'fullName',
+      type: 'text',
+      gridItemProp: { xs: 12 },
+      labelProps: { fontSize: 14, marginBottom: 0 },
+    },
+    {
+      label: 'شماره حساب',
+      name: 'fullName',
+      type: 'text',
+      gridItemProp: { xs: 12 },
+      labelProps: { fontSize: 14, marginBottom: 0 },
+    },
+  ];
+
+  const form = useForm();
+  return (
+    <Stack flexGrow={1} alignItems={'center'}>
+      <Typography variant={'body2'} my={6}>
+        فرم ساز برنامه نویس فرانت
+      </Typography>
+      <InputListWithUseForm
+        inputList={inputList}
+        form={form}
+        gridContainerProps={{ xs: 12 }}
+      />
+      <Button>مشاهده رزومه های دریافتی</Button>
+    </Stack>
+  );
 }

7031590741745217afa50bf6a347e91b9179f8a6
Hossein Khezeli <hosseinkhezli73@gmail.com>
2024-10-02 18:51:47 +0330

    feat:add applicant files

--- src/app/employer/[id]/page.tsx
+++ src/app/employer/[id]/page.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import { SearchInput } from '@/app/components/SearchInput';
 import { PositionList } from '@/app/employer/[id]/components/PositionList';
 
 const Page = ({ params }: { params: { id: string } }) => {
   return (
     <>
-      <SearchInput />
       <PositionList id={params.id} />
     </>
   );

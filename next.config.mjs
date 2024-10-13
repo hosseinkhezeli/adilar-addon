2f192aa11d13c25a50b6a29e5faeb9ba2ebb4ba9
Hossein Khezeli <hosseinkhezli73@gmail.com>
2024-10-13 14:41:22 +0330

    feat:add typed routes to next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { transpilePackages: ['ideep-design-system-2'] };
+const nextConfig = {
+  experimental: {
+    typedRoutes: true,
+  },
+  transpilePackages: ['ideep-design-system-2'],
+};
 
 export default nextConfig;

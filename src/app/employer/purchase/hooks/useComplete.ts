84dd3719ba4bd12ae2e4f8881e65cdba3872e1fd
Hossein Khezeli <hosseinkhezli73@gmail.com>
2024-10-20 11:48:59 +0330

    feat:add success and unsuccess state of payment ui

--- src/app/employer/purchase/hooks/useComplete.ts
+++ src/app/employer/purchase/hooks/useComplete.ts
@@ -8,10 +8,12 @@ import { Route } from 'next';
 //_______________________________________________________________
 
 export function useComplete() {
+  //TODO: make these two dynamic from payment result
   const trackingCode = '458965221214';
+  const isSuccess = false;
   const { push: navigateTo } = useRouter();
   const onClick = () => {
     navigateTo('asdasda/form-creator' as Route);
   };
-  return { onClick, trackingCode };
+  return { onClick, trackingCode, isSuccess };
 }

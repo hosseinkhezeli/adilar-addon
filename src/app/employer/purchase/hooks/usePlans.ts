5f4d1bd504e87f8e24318236f77c8a9ca6c63843
Hossein Khezeli <hosseinkhezli73@gmail.com>
2024-10-21 10:26:50 +0330

    fix:deactivate full plan

--- src/app/employer/purchase/hooks/usePlans.ts
+++ src/app/employer/purchase/hooks/usePlans.ts
@@ -13,23 +13,28 @@ import { Route } from 'next';
 //___________________________________________________________
 
 export function usePlans() {
+  //Dependencies
   const pathname = usePathname();
+  const { setPlan } = usePurchaseStore();
   const { push: navigateTo } = useRouter();
   const searchParams = useSearchParams();
   const postToken = searchParams.get('post_token');
   const advertisementId = searchParams.get('advertisement_id');
+
   const nextState = 'information';
   const newSearchParams = new URLSearchParams({
     state: nextState,
     advertisement_id: advertisementId || '404_advertisement_id',
     post_token: postToken || '404_post_token',
   });
-  const { setPlan } = usePurchaseStore();
+
+  //Handlers
   const handleSubmitPlan = (plan: TPlanCard['plan'] | undefined) => {
     if (plan?.id) {
       setPlan(plan);
       navigateTo(`${pathname}?${newSearchParams.toString()}` as Route);
     }
   };
+
   return { handleSubmitPlan };
 }

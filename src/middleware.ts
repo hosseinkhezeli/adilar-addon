467c28b2128bd76b3331383209374d4b4b2ed6f5
Hossein Khezeli <hosseinkhezli73@gmail.com>
2024-10-21 18:15:24 +0330

    feat:add token from cookie and change authentication method

---
+++ src/middleware.ts
@@ -0,0 +1,25 @@
+// middleware.ts
+import { NextRequest, NextResponse } from 'next/server';
+
+export function middleware(req: NextRequest) {
+  const token = req.nextUrl.searchParams.get('token');
+
+  if (token) {
+    const response = NextResponse.next();
+    response.cookies.set('auth_token', token, {
+      httpOnly: false,
+      secure: process.env.NODE_ENV === 'production',
+      path: '/',
+      maxAge: 60 * 60 * 24, // 1 day
+    });
+    return response;
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    '/((?!api|_next/static|_next/image|video|assets|favicon.ico|sw.js|sitemap.xml|robots.txt).*)',
+  ],
+};

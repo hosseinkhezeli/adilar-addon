badfccf1a2c77299ed4dce87a858456d0e2a7f71
Mostafa <mostafa.fahimipour@gmail.com>
2024-12-01 14:14:47 +0330

    update: add multi buttons for applicant download test

--- src/app/employer/positions/[positionId]/[applicantId]/components/DownloadResume.tsx
+++ src/app/employer/positions/[positionId]/[applicantId]/components/DownloadResume.tsx
@@ -72,6 +72,90 @@ const DownloadResume = ({ resumeId, fullName }: IDownloadResume) => {
           </Typography>
         </Button>
       </Box>
+      <Typography>a tag && without _blank</Typography>
+      <Box
+        component="a"
+        href={`${process.env.NEXT_PUBLIC_API_URL}/api/File/asset/${resumeId}`}
+        download={`${fullName}-resume`}
+      >
+        <Button
+          variant="outlined"
+          endIcon={<DownloadIcon />}
+          sx={{
+            justifyContent: 'space-between',
+            backgroundColor: 'transparent !important',
+            width: '100%',
+          }}
+          // onClick={async () => {
+          //   const res = await fetch(
+          //     `${process.env.NEXT_PUBLIC_API_URL}/api/File/asset/${resumeId}`
+          //   );
+          //   const file = await res.blob();
+
+          //   const url = URL.createObjectURL(file);
+          //   const a = document.createElement('a');
+          //   a.href = url;
+          //   a.download = `${fullName}-resume`;
+          //   document.body.appendChild(a);
+          //   a.click();
+          //   a.remove();
+          //   URL.revokeObjectURL(url);
+          // }}
+        >
+          <Typography
+            variant="caption1"
+            component={'span'}
+            sx={{
+              display: 'inline-flex',
+              alignItems: 'center',
+              gap: 1,
+            }}
+          >
+            <PDFIcon />
+            دانلود فایل رزومه
+          </Typography>
+        </Button>
+      </Box>
+      <Typography>button tag && without _blank </Typography>
+
+      <Button
+        variant="outlined"
+        endIcon={<DownloadIcon />}
+        sx={{
+          justifyContent: 'space-between',
+          backgroundColor: 'transparent !important',
+          width: '100%',
+        }}
+        onClick={async () => {
+          const res = await fetch(
+            `${process.env.NEXT_PUBLIC_API_URL}/api/File/asset/${resumeId}`
+          );
+          const file = await res.blob();
+
+          const url = URL.createObjectURL(file);
+          const a = document.createElement('a');
+          a.href = url;
+          a.download = `${fullName}-resume`;
+          a.type = file.type;
+          document.body.appendChild(a);
+          a.click();
+          a.remove();
+          URL.revokeObjectURL(url);
+        }}
+      >
+        <Typography
+          variant="caption1"
+          component={'span'}
+          sx={{
+            display: 'inline-flex',
+            alignItems: 'center',
+            gap: 1,
+          }}
+        >
+          <PDFIcon />
+          دانلود فایل رزومه
+        </Typography>
+      </Button>
     </Stack>
   );
 };
